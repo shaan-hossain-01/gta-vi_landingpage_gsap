449ae067d5d9e6d815cb09daaf2acbe9d31b8b47
shaan-hossain-01 <rockshaan200@gmail.com>
2025-07-20 00:15:52 +0600

    Complete GSAP methods exercise with play, pause, resume, restart, reverse, kill, repeat, and yoyo functionality

--- pages/02b_understanding-methods/script.js
+++ pages/02b_understanding-methods/script.js
@@ -1,1 +1,43 @@
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+const resume = document.querySelector(".resume");
+const restart = document.querySelector(".restart");
+const reverse = document.querySelector(".reverse");
+const repeat = document.querySelector(".repeat");
+const kill = document.querySelector(".kill");
+const yoyo = document.querySelector(".yoyo");
+
 import gsap from "gsap";
+
+const animation = gsap.to(".box", {
+    opacity: 1,
+    rotation: 360,
+    duration: 2,
+    borderRadius: "50%",
+    scale: 1.25,
+})
+
+play.addEventListener("click", () => {
+    animation.play();
+});
+pause.addEventListener("click", () => {
+    animation.pause();
+});
+resume.addEventListener("click", () => {
+    animation.resume();
+});
+restart.addEventListener("click", () => {
+    animation.restart();
+});
+reverse.addEventListener("click", () => {
+    animation.reverse();
+});
+kill.addEventListener("click", () => {
+    animation.kill();
+});
+repeat.addEventListener("click", () => {
+    animation.repeat(2);
+});
+yoyo.addEventListener("click", () => {
+    animation.yoyo(true);
+});
